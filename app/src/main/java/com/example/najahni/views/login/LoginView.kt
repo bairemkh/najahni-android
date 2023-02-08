d4e9447eb2997f113e103dee490504c8bf575285
bairemkh <72412872+bairemkh@users.noreply.github.com>
2023-02-08 07:27:03 +0100

    testing login

--- app/src/main/java/com/example/najahni/views/login/LoginView.kt
+++ app/src/main/java/com/example/najahni/views/login/LoginView.kt
@@ -2,11 +2,34 @@ package com.example.najahni.views.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.najahni.R
+import com.example.najahni.models.User
 
 class LoginView : AppCompatActivity() {
+    lateinit var viewModel: LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+        viewModel.email.value = findViewById<EditText>(R.id.loginEmail).text.toString()
+        viewModel.password.value = findViewById<EditText>(R.id.loginPassword).text.toString()
+
+    }
+
+    fun login(view: View) {
+        view.isClickable = false
+        viewModel.onLoginClicked()
+        viewModel.loginStatus.observe(this, Observer { result ->
+        when(result){
+            200-> println("ok")
+            else-> println("no")
+        }
+        })
+        view.isClickable = true
     }
 }
--- app/src/main/java/com/example/najahni/views/login/LoginViewModel.kt
+++ app/src/main/java/com/example/najahni/views/login/LoginViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.najahni.views.login
 
-class LoginViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.najahni.models.User
+
+class LoginViewModel: ViewModel() {
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    val loginStatus = MutableLiveData<Int>()
+    fun onLoginClicked(){
+        loginStatus.value = if (email.value == "bairem" && password.value == "bairem") 200 else 404
+    }
 }
