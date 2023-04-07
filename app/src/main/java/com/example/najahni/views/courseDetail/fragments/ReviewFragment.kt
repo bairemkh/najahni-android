7eb50750f94e590dbe7ae79c29bd76ae8db94d00
bairemkh <72412872+bairemkh@users.noreply.github.com>
2023-04-07 09:27:53 +0100

    validation

--- app/src/main/java/com/example/najahni/views/courseDetail/fragments/ReviewFragment.kt
+++ app/src/main/java/com/example/najahni/views/courseDetail/fragments/ReviewFragment.kt
@@ -5,56 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.najahni.R
+import com.example.najahni.models.Course
+import com.example.najahni.utils.Consts
+import com.squareup.picasso.Picasso
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ReviewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ReviewFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class ReviewFragment(val course:Course) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_review, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ReviewFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ReviewFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val view= inflater.inflate(R.layout.fragment_review, container, false)
+        view.findViewById<TextView>(R.id.userNameCourseReview).text = "${course.idOwner.firstname} ${course.idOwner.lastname}"
+        view.findViewById<TextView>(R.id.descriptionCourseReview).text = course.description
+        Picasso.get().load(Consts.BASE_URL1 + course.idOwner.image).into(view.findViewById<ImageView>(R.id.userImgCourseReview))
+        return view
     }
 }
